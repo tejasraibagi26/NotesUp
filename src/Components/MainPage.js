5a5172cfc0d447027d9b22ca4d97fc47248fd0a1
tejasraibagi26 <tejasraibagi@gmail.com>
2021-10-25 13:52:06 -0400

    NotesUp

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -64,7 +64,7 @@ export default function MainPage() {
     }
     console.log(title, content);
     axios
-      .post("http://localhost:5000/notes/add", {
+      .post("https://note-up-server.herokuapp.com/notes/add", {
         title: title,
         content: content,
       })
