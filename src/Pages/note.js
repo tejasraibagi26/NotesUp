f48297f7898cc1d807114293869823d3cda13ea7
tejasraibagi26 <tejasraibagi@gmail.com>
2021-10-26 15:53:19 -0400

    Fixed cors error

--- src/Pages/note.js
+++ src/Pages/note.js
@@ -2,28 +2,24 @@ import axios from "axios";
 import React, { useEffect } from "react";
 
 export default function NoteData() {
+  const [note, setNote] = React.useState([]);
+
   useEffect(() => {
     getDataById();
   });
 
   const getDataById = async () => {
     const id = window.location.pathname.split("/")[2];
-    const { data } = await axios.get(`localhost:5000/notes/${id}`);
-    console.log(data);
+    const { data } = await axios.get(
+      `https://note-up-server.herokuapp.com/notes/${id}`
+    );
+    setNote(data);
   };
 
   return (
     <div>
-      <h1>Note</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <h1>{note.title}</h1>
+      <p>{note.content}</p>
     </div>
   );
 }
